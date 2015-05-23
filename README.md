5a9844425de5d114f5a6ab65c1ac02603e6923cc
hail-seitan <ianlondondu@gmail.com>
2015-05-23 19:25:36 -0400

    not pretty, but functional!

---
+++ README.md
@@ -0,0 +1,3 @@
+###Violin Scales
+
+I couldn't find any good comprehensive fingerboard charts online, so I made this simple JS app.
--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-function nextNoteNumber(noteNum) {
-  noteNum += 1;
-  noteNum %= 12;
-  return noteNum;
-}
-
-function buildScale(startingNote, scaleName) {
-  var scaleArray = [];
-  var scaleFormulas = {
-    "major": [0,2,4,5,7,9,11],
-    "natural minor": [0,2,3,5,7,8,10],
-    "harmonic minor": [0,2,3,5,7,8,11],
-    "melodic minor": [0,2,3,5,7,9,11]
-  };
-
-  if (scaleName in scaleFormulas) {
-    _.forEach(scaleFormulas[scaleName], function(n) {
-      scaleArray.push((startingNote + n) % 12);
-    });
-    return scaleArray;
-  } else {
-    console.log("Error: scale name '" + scaleName + "'not recognized.");
-  }
-
-}
-
 var sharpsByNum = {
   0: 'C',
   1: 'C#',
@@ -74,6 +48,43 @@ var noteNumBySymbol = {
   'B': 11
 };
 
+var scaleFormulas = {
+  "major": [0,2,4,5,7,9,11],
+  "natural minor": [0,2,3,5,7,8,10],
+  "harmonic minor": [0,2,3,5,7,8,11],
+  "melodic minor": [0,2,3,5,7,9,11]
+};
+
+function nextNoteNumber(noteNum) {
+  noteNum += 1;
+  noteNum %= 12;
+  return noteNum;
+}
+
+function buildScale(startingNote, scaleName) {
+  var scaleArray = [];
+
+  if (scaleName in scaleFormulas) {
+    _.forEach(scaleFormulas[scaleName], function(n) {
+      scaleArray.push((startingNote + n) % 12);
+    });
+    return scaleArray;
+  } else {
+    console.log("Error: scale name '" + scaleName + "'not recognized.");
+  }
+}
+
+function showScale(scaleArray) {
+  var inactiveColor = "rgba(255,255,255,0.1)";
+  var activeColor = "rgba(255,255,255,1)";
+  // make all notes invisible, then make only the notes in the scale visible.
+  $(".note").css("color", inactiveColor);
+
+  _.forEach(scaleArray, function(noteNum) {
+    $(".note-" + noteNum).css("color", activeColor);
+  });
+}
+
 function createNoteLiElements(notesObj) {
 // construct the <li>'s for all the notes
   _.forEach(['E','A','D','G'], function(stringName) {
@@ -90,18 +101,32 @@ function createNoteLiElements(notesObj) {
   });
 }
 
-createNoteLiElements(sharpsByNum);
-
-function showScale(scaleArray) {
-  var inactiveColor = "rgba(255,255,255,0.1)";
-  var activeColor = "rgba(255,255,255,1)";
-  // make all notes invisible, then make only the notes in the scale visible.
-  $(".note").css("color", inactiveColor);
-
-  _.forEach(scaleArray, function(noteNum) {
-    $(".note-" + noteNum).css("color", activeColor);
-  });
+function showSelectedScale() {
+  var selectedNote = $('#note-selector').val();
+  var selectedScale = $('#scale-selector').val();
+  showScale(buildScale(noteNumBySymbol[selectedNote],selectedScale));
 }
 
+//===================================================
+// Initial setup
+
+createNoteLiElements(sharpsByNum);
+
+// populate note selector with note names
+_.forEach(_.keys(noteNumBySymbol), function(noteSymbol){
+  $('#note-selector').append(
+    $('<option/>').val(noteSymbol).text(noteSymbol));
+});
+
+// populate scale selector with scale names
+_.forEach(_.keys(scaleFormulas), function(scaleName){
+  $('#scale-selector').append(
+    $('<option/>').val(scaleName).text(scaleName));
+});
+
+
 //example: C maj
-showScale(buildScale(0,"major"));
+// showScale(buildScale(0,"major"));
+
+//examp: G# natural minor
+showScale(buildScale(noteNumBySymbol["G#"],"natural minor"));
